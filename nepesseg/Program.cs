740047acf193a30e148e2997254df8bedbdd57d6
SzBalesz <szarazb@kkszki.hu>
2024-09-17 14:48:18 +0200

    Fájl beolvasása és példányosítás

--- nepesseg/Program.cs
+++ nepesseg/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace nepesseg
 {
@@ -10,7 +11,14 @@ namespace nepesseg
     {
         static void Main(string[] args)
         {
-
+            StreamReader sr = new StreamReader("adatok-utf8.txt");
+            List<Orszag> orszagok = new List<Orszag>();
+            sr.ReadLine(); // fejléc átugrása
+            while (!sr.EndOfStream) 
+            {
+                orszagok.Add(new Orszag(sr.ReadLine()));
+            }
+            Console.ReadKey();
         }
     }
 }
