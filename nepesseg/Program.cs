9193dd47f2ee605a6e9f1234a41935548ccaa9cf
SzBalesz <szarazb@kkszki.hu>
2024-09-17 17:02:19 +0200

    Feladatok átírása eljárásokba, átláthatóság

--- nepesseg/Program.cs
+++ nepesseg/Program.cs
@@ -4,24 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net;
 
 namespace nepesseg
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static List<Orszag> orszagok = new List<Orszag>();
+        static void Beolvasas()
         {
             StreamReader sr = new StreamReader("adatok-utf8.txt");
-            List<Orszag> orszagok = new List<Orszag>();
             sr.ReadLine(); // fejléc átugrása
-            while (!sr.EndOfStream) 
+            while (!sr.EndOfStream)
             {
                 orszagok.Add(new Orszag(sr.ReadLine()));
             }
-            Console.WriteLine("4. feladat");
             Console.WriteLine($"A beolvasott országok száma: {orszagok.Count}.");
-            Console.WriteLine();
-            Console.WriteLine("5. feladat");
+            sr.Close();
+        }
+        static void KinaNepsurusege()
+        {
             foreach (var o in orszagok)
             {
                 if (o.Orszagnev == "Kína")
@@ -29,8 +31,9 @@ namespace nepesseg
                     Console.WriteLine($"Kína népsűrűsége: {o.Nepsuruseg()} fő/km^2.");
                 }
             }
-            Console.WriteLine();
-            Console.WriteLine("6. feladat");
+        }
+        static void KinaEsIndia()
+        {
             int kinaL = 0;
             int indiaL = 0;
             foreach (var o in orszagok)
@@ -39,14 +42,15 @@ namespace nepesseg
                 {
                     kinaL = o.Nepesseg;
                 }
-                if(o.Orszagnev == "India")
+                if (o.Orszagnev == "India")
                 {
                     indiaL = o.Nepesseg;
                 }
             }
-            Console.WriteLine($"Kinában a lakosság {kinaL-indiaL} fővel volt több.");
-            Console.WriteLine();
-            Console.WriteLine("7. feladat");
+            Console.WriteLine($"Kinában a lakosság {kinaL - indiaL} fővel volt több.");
+        }
+        static void HarmadikLegnepszerubb()
+        {
             int harmadikIndex = 0;
             for (int i = 0; i < orszagok.Count; i++)
             {
@@ -56,7 +60,9 @@ namespace nepesseg
                 }
             }
             Console.WriteLine($"A harmadik legnépesebb ország: {orszagok[harmadikIndex].Orszagnev}, a lakosság {orszagok[harmadikIndex].Nepesseg} fő.");
-            Console.WriteLine("8. feladat - A következő országok lakosságának több mint 30%-a a fővárosban lakik:");
+        }
+        static void Meghaladja()
+        {
             foreach (var o in orszagok)
             {
                 if (o.Meghaladja())
@@ -64,6 +70,28 @@ namespace nepesseg
                     Console.WriteLine($"\t{o.Orszagnev} ({o.Fovaros})");
                 }
             }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("4. feladat");
+            Beolvasas();
+            Console.WriteLine();
+
+            Console.WriteLine("5. feladat");
+            KinaNepsurusege();
+            Console.WriteLine();
+
+            Console.WriteLine("6. feladat");
+            KinaEsIndia();
+            Console.WriteLine();
+
+            Console.WriteLine("7. feladat");
+            HarmadikLegnepszerubb();
+            Console.WriteLine();
+
+            Console.WriteLine("8. feladat - A következő országok lakosságának több mint 30%-a a fővárosban lakik:");
+            Meghaladja();
+
             Console.ReadKey();
         }
     }
